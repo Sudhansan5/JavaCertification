2a90077b88ecae427c39315f60dd7654ddbf209f
Sudhanshu Kumar <sudhansan5@gmail.com>
2023-08-19 15:31:39 +0530

    Test commit

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    int method( int num){
+         int y = 5;
+        return y;
+    }
+    public static void main(String[] args) {
+        System.out.println("hi");
+
+    }
+}
